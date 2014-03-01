ffdba5ab03515dbafb19bd237c9da3c95b99ef3b
fullerf <fullerf@umich.edu>
2014-02-28 20:55:13 -0500

    tweaks to new afd algorithm

--- afd_time_domain.jl
+++ afd_time_domain.jl
@@ -1,20 +1,35 @@
 function psiNext(p::Vector,psi::Vector)
+    psi = cat(1,zero(Complex128),psi)
     N = length(psi)
     psik = zero(psi)
     if length(p)==1
         p = cat(1,zero(p[1]),p)
         c = sqrt(1-abs2(p[end]))
+        #in this case we assume psi = u(t), rather than the previous psi
+        for j=0:(N-1)
+            psik[j+1] = c*sum([p[end]^(i)*psi[j-i+1] for i=0:(j-1)])
+        end
     else
         c = sqrt((1-abs2(p[end]))/(1-abs2(p[end-1])))
+        for j=0:(N-1)
+            psik[j+1] = c*sum([p[end]^(i)*(psi[j-i]-conj(p[end-1])*psi[j-i+1]) for i=0:(j-1)])
+        end
     end
-    for j=2:(N)
-        psik[j] = c*sum([p[end]^(j-i)*(psi[i-1]-conj(p[end-1])*psi[i]) for i=2:j])
-    end
-    return psik
+    return psik[2:end]
 end
 
-function pulseFun(K::Int)
-    return cat(1,one(Complex128),zeros(Complex128,K-1))
+function genPsiBasis(p::Vector,u::Vector)
+    N = length(p)
+    B = zeros(Complex128,(length(u),N+1))
+    B[:,1] = u
+    for k=1:N
+        B[:,k+1] = psiNext([p[1:k]],B[:,k])
+    end
+    return B[:,2:end]
+end
+
+function pulseFun(k::Int)
+    return cat(1,one(Complex128),zeros(Complex128,k-1))
 end
 
 function unitStep(k::Int)
@@ -24,22 +39,6 @@ end
 function dictElem(p::Complex128,K::Int)
     x = [(sqrt(1-abs2(p))*(p^(k-1))*unitStep(k-1))::Complex128 for k=0:(K)]
     return x[2:end]
-end
-
-function zV(V::Vector,K::Int)
-    return V[K+1]
-end
-
-function filterGk(p::Complex128,Gk::Vector,A::Complex128)
-    ai = 1/conj(p)
-    Gk = cat(1,zero(Complex128),Gk)
-    N = length(Gk)
-    Gkp1 = zero(Gk)
-    for k=0:(N-1)
-        Gkp1[k+1]  = sum([ai^(i+1)*(p*Gk[k-i]-Gk[k-i+1]) for i=0:(k-1)])
-        Gkp1[k+1] += A*sqrt(1-abs(p))*(ai^k)*unitStep(k-1)
-    end
-    return Gkp1[2:end]
 end
 
 function filterR(p::Complex128,R::Vector)
@@ -103,10 +102,27 @@ function afdT(target::Vector,Npoles::Int,Nstarts::Int)
         ep = dictElem(p[j],length(target))
         a[j] = dot(ep,gk)
         gk = filterR(p[j],(gk-a[j]*ep))
-        figure()
-        plot(gk)
     end
     return (p,a)
+end
+
+function optimalAfdTruncation(poles::Vector{Complex128},amps::Vector{Complex128},xn::Vector{Complex128})
+    L = length(poles)
+    Nt = length(xn)
+    if length(amps)!=L
+        error("# of poles should be the same as number of amplitudes")
+    end
+    infVec = zeros(Float64,L)
+    for k=1:L
+        model = [reconstructTimeDomain(poles[1:k],amps[1:k],i)::Complex128 for i=linspace(1,Nt,Nt)]
+        rssval = residualSumOfSquares(model,xn)
+        # one complex value = 2 parameters, so each pole and amp is 4 parameters, each complex valued input
+        # is worth 2 data points of input in this thinking, thus:
+        infVec[k] = akaikeInformationCriterion(rssval,4*k,2*length(xn))
+    end
+    infMinInd = indmin(infVec)
+    aicVec = exp((-infVec+infVec[infMinInd])/2)
+    return indmax(aicVec)
 end
 
 
