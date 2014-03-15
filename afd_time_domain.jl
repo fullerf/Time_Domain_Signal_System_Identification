58181aa9d7a742ca22937cd74589bb4a9fb50786
fullerf <fullerf@umich.edu>
2014-03-15 19:41:22 -0400

    fftaxis lolz

--- afd_time_domain.jl
+++ afd_time_domain.jl
@@ -328,5 +328,17 @@ function cubeMatrix(M::Array,N::Int)
     return cM
 end
 
+function fftaxis(N::Int,TSpac=1.0)
+    if abs(mod(N,2))>0
+        a = [0:1:int((N-1)/2)]
+        b = [-(N-1)/2:1:-1]
+        r = cat(1,a,b)*(N*TSpac)
+    else
+        a = [0:1:int(N/2-1)]
+        b = [-int(N/2):1:-1]
+        r = cat(1,a,b)*(N*TSpac)
+    end
+    return r
+end
 
 
